c5dc51fb30c8acfbdca6ffefdedc68edace76ca6
Louise Benson <louise.benson@gmail.com>
2023-09-08 14:55:59 +0000

    Add checkout views and templates

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
-from .forms import OrderFrom
+from .forms import OrderForm
 
 # Create your views here.
 
