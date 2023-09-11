9e7c4e6a000fccfc55fb87c077c3d6f6c77f5caa
Louise Benson <louise.benson@gmail.com>
2023-09-11 10:58:46 +0000

    Add order summary to checkout success page

--- bag/views.py
+++ bag/views.py
@@ -63,7 +63,7 @@ def adjust_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size'][size]:
                 bag.pop(item_id)
-                messages.success(request, f'Removed {size.upper()} {product.name} from your bag')
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -89,10 +89,10 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed {size.upper()} {product.name} from your bag')
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
         else:
             bag.pop(item_id)
-        messages.success(request, f'Removed {product.name} from your bag')
+            messages.success(request, f'Removed {product.name} from your bag')
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
